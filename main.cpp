73a0a77a5eba27e5bfd2da506bd1038fd567008b
SkyIceWall <62433628+SkyIceWall@users.noreply.github.com>
2023-04-18 21:00:53 +0200

    Modification de valide(), harmonisation/ suppression des conflits de main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ struct instance
     int ** matrice;
 };
 
-struct solution
-{
-    int taille,score;
+struct solution {
+
+    int taille, score;
     char ** matrice;
 };
 
@@ -41,7 +41,7 @@ void initialisationInstance (string ficA, instance & g) // Initialise l'instance
     else cerr << "Erreur : le fichier ne peut pas être lu !";
 }
 
-void suppressionInstance (instance g)
+void suppressionInstance (instance & g)
 {
     for (int i = 0; i < g.taille; ++i)
         delete[] g.matrice[i];
@@ -59,54 +59,40 @@ void affichageInstance (instance g)
         for (int j = 0; j < t; ++j)
             cout << g.matrice[i][j] << ' ';
     }
+    cout << endl;
 }
 
-bool valideSolution(solution s)
+bool valide (solution s)
 {
-    int compteur_r=0,i=0,j=0;
-    while(i< s.taille and compteur_r<=1)
-    {
-<<<<<<< Updated upstream
-        cout << endl;
-        for (int j = 0; j < t; ++j)
-            cout << s.matrice[i][j] << ' ';
-    }
-    // cout << endl << s.score << endl;
-}
-
-bool valide (solution s, int t)
-{
+    int t = s.taille;
     int r = 0;
     int i = 0, j = 0;
 
     while (r < 2 and i < t)
     {
-        while (j < t)
+        do
         {
-            if (s.matrice[i][j] == 'R') r++;
-            ++j;
+            if (s.matrice[i][j++] == 'R') r++;
         }
-        j = 0;
-        ++i;
-    }
-    if (r != 1) return false;
-    return true;
-=======
-        if (s.matrice[i][j]== 'R')compteur_r++;
-        j++;
-        if (j==s.taille)
+        while (r < 2 and j < 4);
+
+        if (r < 2)
         {
-            j=0;
+            j = 0;
             ++i;
         }
     }
-    return compteur_r==1;
->>>>>>> Stashed changes
+    // cout << endl << r << ' ' << i << ' ' << j << endl;
+    return r == 1;
 }
 
-void genereSolution (solution & s,int t)
+void genereSolution (solution & s, instance g)
 {
+    int t = g.taille;
+    s.taille = t;
+    
     srand(time(NULL));
+    
     char lettres[] = {'J', 'V', 'N', 'B', 'O', 'R'}; 
 
     s.matrice = new char * [t];
@@ -121,82 +107,78 @@ void genereSolution (solution & s,int t)
                 s.matrice[i][j] = lettres[rand() % 6];
             }
         }
-    } while (!valideSolution(s));
+    } while (!valide(s));
 }
 
 void affichageSolution (solution s)
 {
     int t = s.taille;
+
     for (int i = 0; i < t; ++i)
     {
         cout << endl;
         for (int j = 0; j < t; ++j)
             cout << s.matrice[i][j] << ' ';
     }
+    cout << endl;
     // cout << endl << s.score << endl;
 }
 
-void suppressionSolution (solution g)
+void suppressionSolution (solution & s)
 {
-    for (int i = 0; i < g.taille; ++i)
-        delete[] g.matrice[i];
-    delete[] g.matrice;
-}
-
-void SaisieSolution(solution & s)
-{
-    cout<<"Quelle est la taille de la matrice solution ? ";
-    cin>>s.taille;
-    s.matrice = new char * [s.taille];
-    for (int i = 0; i < s.taille; ++i) s.matrice[i] = new char [s.taille];
-
     for (int i = 0; i < s.taille; ++i)
-    {
-        for (int j = 0; j < s.taille; ++j)
-        {
-            cin>>s.matrice[i][j];
-        }
-    }
+        delete[] s.matrice[i];
+    delete[] s.matrice;
 }
 
-int calculpoint(instance g,solution s)
-{
-    int scoreTotal=0;
+//void saisieSolution(solution & s, instance g)
+//{
+//    t = g.taille;
+//    s.taille = t;
+//    
+//    s.matrice = new char * [t];
+//    for (int i = 0; i < t; ++i) s.matrice[i] = new char [t];
+//
+//    for (int i = 0; i < t; ++i)
+//    {
+//        for (int j = 0; j < t; ++j)
+//        {
+//            cin >> s.matrice[i][j];
+//        }
+//    }
+//}
 
-    for (int i=0;i<s.taille;++i)
-    {
-        for (int i j=0;j<s.taille;++j)
-        {
-            val_case=g.matrice[i][j];
-            car_case=s.matrice[i][j];
+// int calculeScore (instance g, solution s)
+// {
+//     int scoreTotal=0;
 
-            if (car_case='J') //Jaune
-            {
+//     for (int i=0;i<s.taille;++i)
+//     {
+//         for (int i j=0;j<s.taille;++j)
+//         {
+//             val_case=g.matrice[i][j];
+//             car_case=s.matrice[i][j];
+
+//             if (car_case='J') //Jaune
+//             {
                 
-            }
-        }
-    }
-}
+//             }
+//         }
+//     }
+// }
+
 int main ()
 {
-<<<<<<< Updated upstream
-    string repertoire = "instances hors-compétition (15)/probleme_4_a.txt";
+    string repertoire = "Problemes/probleme_4_a.txt";
     instance a;
     solution b;
 
-    initialisation(repertoire, a);
+    initialisationInstance(repertoire, a);
     affichageInstance(a);
-    genereSolution(b, a.taille);
-    affichageSolution(b, a.taille);
-    cout << endl << valide(b, a.taille) << endl;
-
-=======
-    //string repertoire = "problemes/probleme_4_a.txt";
-    //instance a;
-    solution s;
-    SaisieSolution(s);
-    affichageSolution(s);
-    cout<<"Solution valide ?"<<valideSolution(s);
->>>>>>> Stashed changes
-    return 0;
+    genereSolution(b, a);
+    affichageSolution(b);
+    valide(b) == 1 ? cout << endl << "Solution valide." << endl : cout << endl << "Solution invalide." << endl;
+    
+    suppressionInstance(a);
+    suppressionSolution(b);
 }
