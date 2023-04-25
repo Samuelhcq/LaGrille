03f289fd786d0ec661081df2f1bbbd812c17d07c
Alexandre-lr <alexandre.leroux4919@gmail.com>
2023-04-25 12:36:13 +0200

    Ajout du score des pions jaunes, ajout de commentaire

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+//??
 #include <cstdlib>
+//Pour la génération de grille aléatoire
 #include <ctime>
+//Pour clear le terminal
+#include <stdlib.h>
+//Pour renvoyer des tuples
 #include <tuple>
 
 using namespace std;
@@ -42,11 +47,15 @@ void initialisationInstance (string ficA, instance & g) // Initialise l'instance
     else cerr << "Erreur : le fichier ne peut pas être lu !";
 }
 
-void suppressionInstance (instance & g)
+void suppressionInstance(instance & g)
 {
-    for (int i = 0; i < g.taille; ++i) //A MODIFIER !!!!!!!!!!!!!!!!
+    for (int i = 0; i < g.taille; ++i)
         delete[] g.matrice[i];
     delete[] g.matrice;
+
+    g.taille = 0;
+    g.penalite = 0;
+    g.matrice = nullptr;
 }
 
 void affichageInstance (instance g)
@@ -127,9 +136,13 @@ void affichageSolution (solution s)
 
 void suppressionSolution (solution & s)
 {
-    for (int i = 0; i < s.taille; ++i)
+    for (int i = 0; i <s.taille; ++i)
         delete[] s.matrice[i];
     delete[] s.matrice;
+
+    s.taille=0;
+    s.score=0;
+    s.matrice=nullptr;
 }
 
 //void saisieSolution(solution & s, instance g)
@@ -151,14 +164,14 @@ void suppressionSolution (solution & s)
 
 bool pionOrthogonal (solution s, char l, int i, int j)
 {
-    // au dessus
-    if (i > 0 && s.matrice[i-1][j] == l) return true;
-    //en dessous
-    if (i < s.taille-1 && s.matrice[i+1][j] == l) return true;
     // à gauche
     if (j > 0 && s.matrice[i][j-1] == l) return true;
     //à droite
     if (j < s.taille-1 && s.matrice[i][j+1] == l) return true;
+    // au dessus
+    if (i > 0 && s.matrice[i-1][j] == l) return true;
+    //en dessous
+    if (i < s.taille-1 && s.matrice[i+1][j] == l) return true;
 
     return false;
 }
@@ -323,14 +336,119 @@ int pionsNoirs (solution s, instance g)
     return score;*/
 }
 
+//int si on return le score avec les penalités
+tuple<int, int> pionsJaunes (solution s, instance g)
+{
+    int t = s.taille, nbPions = compteurPions(s, 'J'), score = 0, nbPionsIsoles = 0;
+
+    int i = 0, j = 0;
+    //Algorithme 1, calcule score et penalité en meme temps, pas de 2 en colonne, 1 en ligne
+    while (!(nbPions == 0) and i < t)
+    {
+        while (!(nbPions == 0) and j < t)
+        {
+            //Si la case est jaune
+            if (s.matrice[i][j] == 'J')
+            {
+                //Ajout du score
+                score += g.matrice[i][j];
+                //Verification des cases adjacentes orthogonalement ou (inclusif) en diagonale
+                //Si true on ajoute un pion isolé et on passe 1 case, sinon on passe 2 cases (pas de jaune autour)
+                if (!(pionOrthogonal(s, 'J', i, j) or pionDiagonal(s, 'J', i, j)))
+                {
+                    //Ajout d'un pion isolé
+                    nbPionsIsoles++;
+                    //Sinon on passe 0 case (+1, en dehors de la condition)
+                }
+                //Sinon on passe 1 case (+1, en dehors de la condition)
+                else ++j;
+                --nbPions;
+            }
+            //On passe 1 case
+            j++;
+        }
+        ++i;
+        j = 0;
+    }
+
+    //Algorithme 2, calcule du score et calcule du nombre de pion isolé séparés, pas de 2 en colonne et ligne
+    /*while (!(nbPions == 0) and i < t)
+    {
+        while (!(nbPions == 0) and j < t)
+        {
+            //Si la case est jaune
+            if (s.matrice[i][j] == 'J')
+            {
+                cout << "Jaune ! " << i << ' ' << j << ' ' << g.matrice[i][j] << endl;
+                //Ajout du score
+                score += g.matrice[i][j];
+                --nbPions;
+            }
+            j++;
+        }
+
+        if (!(nbPions == 0))
+        {
+            i++;
+            j = 0;
+        }
+    }
+
+    cout << i << ' ' << j <<endl;
+
+    nbPions = compteurPions(s, 'J'), i = 0, j = 0;
+    
+    while (!(nbPions == 0) and i < t)
+    {
+        // cout << "ligne " << i << endl;
+        while (!(nbPions == 0) and j < t)
+        {
+            // cout << "-colonne " << j <<endl;
+            //Si la case est jaune
+            if (s.matrice[i][j] == 'J')
+            {
+                // cout << "--trouvé ! " << i <<' ' << endl;
+                //Verification des cases adjacentes orthogonalement ou (inclusif) en diagonale
+                //Si true on ajoute un pion isolé puis on passe 2 cases (on ignore les scores)
+                if (!(pionOrthogonal(s, 'J', i, j) or pionDiagonal(s, 'J', i, j)))
+                {
+                    // cout << "---isolé" << " pion adjacent ortho=" << pionOrthogonal(s, 'J', i, j) << ' ' << " pion adjacent diago=" << pionDiagonal(s, 'J', i, j) << endl;
+                    //Ajout d'un pion isolé
+                    nbPionsIsoles++;
+                    //Sinon on passe 2 case (+1, en dehors de la condition)
+                    j++;
+                }
+                --nbPions;
+            }
+            j++;
+        }
+        i++;
+        j = 0;
+    }*/
+
+    //Renvoie du score avec les pénalités appliqués
+    // cout << "Score pions jaunes : " << score - nbPionsIsoles * g.penalite << "\tParamètres : " << "somme des pions=" << score << "\t, nombre de pion isolé=" << nbPionsIsoles << endl;
+    // return score - nbPionsIsoles * g.penalite;
+
+    //Revoie d'un tuple avec le score et le nombre de pénalité
+    cout << "Projection du score pions jaunes : " << score - nbPionsIsoles * g.penalite << "\tParamètres : " << "somme des pions=" << score << "\t, nombre de pion isolé(nombre de pénalité)=" << nbPionsIsoles << endl;
+    return make_tuple(score, nbPionsIsoles);
+}
+
 void calculeScore (solution & s, instance g)
 {
     int score = 0;
     int nbPenalite = 0;
+    //A supprimer si on utilise pas les tuples
+    int tempScore = 0, tempNbPenalite = 0;
 
     nbPenalite += pionsBleus (s, g); //Pion bleus = pénalité seulement, score = 0
     score += pionRouge (s, g); //Pion rouge = score seulement, aucune pénalité
     score += pionsNoirs (s, g); //Pion noirs = score seulement, aucune pénalité
+    // nbPenalite += pionsOranges(s, g); //Pion bleus = pénalité seulement, score = 0
+    //A modifier si on utilise pas les tuples
+    tie(tempScore, tempNbPenalite) = pionsJaunes(s, g);
+    score += tempScore; nbPenalite += tempNbPenalite;
 
     // cout << "debug calculeScore : " << "nombre de pénalités = " << nbPenalite << ' ' << "valeur des pénalités = " << g.penalite << endl;
     s.score = score - (nbPenalite * g.penalite);
@@ -338,6 +456,7 @@ void calculeScore (solution & s, instance g)
 
 int main ()
 {
+    system ("CLS");
     string repertoire = "Instances/probleme_4_a.txt";
     instance a ;
     solution b;
