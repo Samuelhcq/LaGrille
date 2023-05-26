46e27043bae06afba23895604d594ada152406ee
SkyIceWall <62433628+SkyIceWall@users.noreply.github.com>
2023-05-26 18:02:55 +0200

    Amélioration de l'experience utilisateur

--- README.md
+++ README.md
@@ -6,16 +6,23 @@ Comment télécharger l'archive ?
 Pour lancer le programme, suivez les instructions suivantes :
 1. Télécharger le .zip du projet
 2. Ouvrir un terminal où le CMakeLists.txt se trouve
-3. Saisissez la commande ```mkdir build && cd build```, puis ```cmake``` et ```make```
+3. Saisissez la commande ```mkdir build && cd build``` , puis ```cmake``` et ```make```
 
 L'executable à du être créé sous le nom de *lagrille*.out
 
-Comment choisir l'instance et lancer l'algorithme ?
+Comment lancer l'algorithme ?
 -----------------------------------
 Après la création de votre executable (cf . *lagrille*.out) vous pouvez maintenant choisir sur quelle grille lancer l'algorithme mais aussi où va aller la solution généré !
-### Choisir l'instance
-1. Ouvrir le fichier ```main.cpp``` dans le dossier ```programme```
-2. Modifier le chemin relatif du fichier en question dans la variable ```cheminInstance``` (ligne 33)
+1. Ouvrir un terminal dans ```build```
+2. Lancer la commande ```./lagrille``` suivis des instance et des sortis des solutions que vous souhaitez calculer
 
-### Choisir où va aller le fichier de sortie
-Dans le meme fichier (ie. ```main.cpp```) vous pouvez modifier le chemin relatif et nom de fichier de sortie dans la variable ```cheminSortie``` (par defaut ```../sortie.txt```)
+### Exemple
+>```console
+>.\lagrille "../../instances/competition_01.txt" "../../sortie_01.txt" 
+>```
+>Calcule la solution de l'instance ```competition_01.txt``` situé dans ```../../instances/```, puis l'écrit dans le fichier texte ```sortie_01.txt``` situé dans ```../../```
+
+>```console
+>.\lagrille "../../instances/competition_01.txt" "../../sortie_01.txt" "../../instances/competition_02.txt" "../../sortie_02.txt" "../../instances/competition_03.txt" "../..>/sortie_03.txt" "../../instances/competition_04.txt" "../../sortie_04.txt" "../../instances/competition_05.txt" "../../sortie_05.txt" "../../instances/competition_06.txt" >"../../sortie_06.txt"  "../../instances/competition_07.txt" "../../sortie_07.txt" "../../instances/competition_08.txt" "../../sortie_08.txt" "../../instances>/competition_09.txt" "../../sortie_09.txt" "../../instances/competition_10.txt" "../../sortie_10.txt" "../../instances/competition_11.txt" "../../sortie_11.txt" "../..>/instances/competition_12.txt" "../../sortie_12.txt"
+>```
+>Calcule la solution des 12 intances de compétition
--- programme/main.cpp
+++ programme/main.cpp
@@ -22,45 +22,50 @@ void clear()
 #endif
 }
 
-int main ()
+int main (int argc, char ** argv)
 {
-    clock_t depart = clock();
-    cout << depart << endl;
     //Nettoyage du termnial (affichage propre)
     clear();
+    cout << "Terminal nettoyé..." << endl;
 
     //Chemins d'accès au fichier d'entrée
-    string cheminInstance, cheminSolution, cheminSortie;
-    cheminInstance = "../instances/competition_12.txt", cheminSortie = "../sortie.txt";
+    string cheminInstance, cheminSortie;
 
-    //Création de l'instance (fichier d'entrée)
-    instance a;
-    //Création de la solution (fichier de sortie)
-    solution b;
+    cout << "Lancement du programme..." << endl;
+    for (int i = 1; i < argc*2; i += 2)
+    {
+        clock_t depart = clock();
+        cheminInstance = argv[i], cheminSortie = argv[i+1];
 
-    //Initialisation de la grille de l'instance
-    initialisationInstance(cheminInstance, a);
-    //Achiffage du chemin d'accès de l'instance
-    cout << cheminInstance << endl;
-    //Affichage de la instance dans le terminal
-    affichageInstance(a);
+        //Création de l'instance (fichier d'entrée)
+        instance a;
+        //Création de la solution (fichier de sortie)
+        solution b;
 
-    //Initialisation de la solution via l'algorithme
-    b.taille = a.taille;
-    //Calcule de la grille solution
-    b.matrice = Boptimal(a);
-    //Calcule du score de la solution
-    calculeScore(b, a);
-    //Achiffage du chemin d'accès de la solution
-    cout << cheminSortie << endl;
-    //Affichage de la solution dans le terminal
-    affichageSolution(b);
+        //Initialisation de la grille de l'instance
+        initialisationInstance(cheminInstance, a);
+        //Achiffage du chemin d'accès de l'instance
+        cout << cheminInstance << endl;
+        //Affichage de la instance dans le terminal
+        affichageInstance(a);
 
-    clock_t fin = clock();
-    cout << endl << "Temps d'exécution : "<< fin/CLOCKS_PER_SEC << " secondes." << endl;
-    
-    //Ecriture dans un fichier de la solution
-    ecriture(cheminSortie, b);
+        //Initialisation de la solution via l'algorithme
+        b.taille = a.taille;
+        //Calcule de la grille solution
+        b.matrice = Boptimal(a);
+        //Calcule du score de la solution
+        calculeScore(b, a);
+        //Achiffage du chemin d'accès de la solution
+        cout << cheminSortie << endl;
+        //Affichage de la solution dans le terminal
+        affichageSolution(b);
+
+        clock_t fin = clock();
+        cout << endl << "Temps d'exécution : "<< (0.0 + fin - depart)/CLOCKS_PER_SEC << " secondes." << endl;
+        
+        //Ecriture dans un fichier de la solution
+        ecriture(cheminSortie, b);
+    }
 
     return 0;
 }
